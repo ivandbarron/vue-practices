c8dffba09ac6d90133ff60e004d43cc2fc80a910
Ivan David Barron <david@bunsan.io>
2020-03-22 18:41:04 -0600

    Se agrega tarea 2

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Language title="Python" use-cases="Data Science, Videogames, General purpose, NetOps" />
+    <Language title="PHP" use-cases="Server side scripting" />
+  </div>
+</template>
+
+<script>
+import Language from './components/Language.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Language
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Language.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="language">
+    <h1 :class="[ 'normal', { isInactive: showMoreSection }]">{{ title }}</h1>
+    <p>
+      {{ description }}
+    </p>
+    
+    <a v-show="!showMoreSection" @click.prevent="showMoreSection = !showMoreSection" href="#">READ MORE...</a>
+    
+    <div v-show="showMoreSection" class="more-section">
+      <a @click.prevent="showMoreSection = !showMoreSection" href="#">HIDE...</a>
+
+      <h3>Use cases</h3>
+      <span>
+        {{ useCases }}
+      </span>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+function validate_string(value) {
+  return (typeof value === 'string' || value instanceof String)
+}
+
+function lorem_ipsum() {
+  return "\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis \
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore \
+eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
+sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
+
+export default {
+  name: "Language",
+  data: function() {
+    return {
+      showMoreSection: false
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Title",
+      validator: function(value) {
+        return validate_string(value)
+      }
+    },
+    description: {
+      type: String,
+      default: lorem_ipsum(),
+      validator: function(value) {
+        return validate_string(value)
+      }
+    },
+    useCases: {
+      type: String,
+      default: "No use cases",
+      validator: function(value) {
+        return validate_string(value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.language {
+  width: 200px;
+  float: left;
+  margin: 8px;
+}
+
+.normal {
+  color: goldenrod;
+}
+
+.isInactive {
+  color: greenyellow;
+}
+
+
+</style>
